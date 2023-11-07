00f62fabde39c5f0c61f79a9cefc0e44c6792d6d
unknown <skyevin7@gmail.com>
2023-11-07 08:50:59 +0700

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./hero";
+import PopularProducts from "./popularproducts";
+import SuperQuality from "./superquality";
+import Services from "./services";
+import SpecialOffer from "./specialoffer";
+import CustomerReviews from "./customerreviews";
+import Subscribe from "./subscribe";
+import Footer from "./footer";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+};
